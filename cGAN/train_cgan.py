bae339fa856e14b8d0e42ce6c371bfbc08c5ce09
Katarzyna Michalowska <katarzyna.michalowska@sintef.no>
2024-09-11 13:43:58 +0200

    Renamed some parameters, simplified the optimizer logic, added loading pretrained generator logic

--- cGAN/train_cgan.py
+++ cGAN/train_cgan.py
@@ -10,6 +10,7 @@ from modules.data_manipulation import preprocess_data_for_model
 from modules.data_loading import load_preprocessed_data
 from modules import log_functions
 from modules.plotting import plot_gan_samples
+from modules.train_utils import get_optimizer
 import modules.model_definitions_pytorch as md
 import torch
 import torch.nn as nn
@@ -26,7 +27,7 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 with open(os.path.join(script_dir, 'params_train_cgan.yaml'), 'r') as file:
     params = dict(yaml.safe_load(file))
 
-main_model_folder = params.get('model_dir')
+main_model_folder = params.get('save_model_dir')
 output_folder = f"{main_model_folder}/cgan_{timestamp_now()}"
 
 if not os.path.exists(output_folder):
@@ -51,36 +52,48 @@ else:
     raise ValueError("The 'datasets' parameter must be a list of dataset directories.")
 
 variance_y = np.var(Y)
-checkpoint_dir = os.path.join(output_folder, "cp")
+cp_dir = os.path.join(output_folder, "cp")
 logs_path = os.path.join(output_folder, "log.out")
 logs = open(logs_path, 'w')
 logs.write(log_functions.print_time("Program started"))
 
-# Load models
 num_samples = X.shape[0]
 timesteps = X.shape[1]
 condition_dim = X.shape[2]
 input_channels = Y.shape[2] 
 
+# Generator
 generator = md.Generator(input_features=X.shape[2], hidden_dim=params.get('hidden_dim'), noise_dim=params.get('noise_vector_dim'), 
                          timesteps=params.get('t_len'))
 
-if params.get("model_preload") is not None:
-    weigths_path = os.path.join(main_model_folder, params.get('model_preload'), "checkpoints", f"cp-{params.get('model_preload_cp'):04d}.pth")
-    if device==torch.device('cuda'):
-        generator.load_state_dict(torch.load(weigths_path))
-    else:
-        generator.load_state_dict(torch.load(weigths_path, map_location=torch.device('cpu')))
+# Load weights of pretrained generator
+if (params.get('load_generator_path') is not None) and (params.get('load_generator_cp') is not None):
+    weigths_path = os.path.join(params.get('load_generator_path'), "cp", f"cp-{params.get('load_generator_cp'):04d}.pth")
+    generator.load_state_dict(torch.load(weigths_path, map_location=device))
 
-if params.get('discr_input') == "ts":
-    discriminator = md.Discriminator(input_channels, condition_dim)
-    
-elif params.get('discr_input') == "fft":
-    discriminator = md.DiscriminatorFFTInput(input_channels, condition_dim)
+# Discriminator
+# TODO: This can load one model with different parameters.
+if params.get('skip_connections'):
+    if params.get('discriminator_input') == "ts":
+        discriminator = md.DiscriminatorWithSkipConnections(input_channels, condition_dim)
+    elif params.get('discriminator_input') == "fft":
+        discriminator = md.DiscriminatorFFTInputWithSkipConnections(input_channels, condition_dim, use_log=params.get('discriminator_use_log'))
+else:
+    if params.get('discriminator_input') == "ts":
+        discriminator = md.Discriminator(input_channels, condition_dim)
+    elif params.get('discriminator_input') == "fft":
+        discriminator = md.DiscriminatorFFTInput(input_channels, condition_dim, use_log=params.get('discriminator_use_log'))
 
-optimizer_G = torch.optim.Adam(generator.parameters(), lr=params.get('lr_G'), betas=(0.5, 0.999))
-optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=params.get('lr_D'), betas=(0.5, 0.999))
-criterion = nn.BCELoss()
+# Optimizers
+
+
+optimizer_G = get_optimizer(params.get("optimizer_G"), generator, params.get('lr_G'))
+optimizer_D = get_optimizer(params.get("optimizer_D"), discriminator, params.get('lr_D'))
+
+
+
+# Loss function
+adv_loss = nn.BCELoss()
 
 generator.to(device)
 discriminator.to(device)
@@ -99,8 +112,8 @@ nr_batches = len(dataloader)
 
 if not os.path.exists(os.path.join(output_folder, "plots")):
     os.makedirs(os.path.join(output_folder, "plots"))
-if not os.path.exists(checkpoint_dir):
-    os.makedirs(checkpoint_dir)
+if not os.path.exists(cp_dir):
+    os.makedirs(cp_dir)
 
 
 # Training
@@ -122,56 +135,86 @@ for epoch in range(1, params.get('n_epochs')+1):
         optimizer_D.zero_grad()
         generated_data = generator(conditions)
 
-        real_loss = criterion(discriminator(real_data, conditions), valid)
-        fake_loss = criterion(discriminator(generated_data.detach(), conditions), fake)
+        if (params.get('lambda_adversarial') is not None) and (params.get('lambda_adversarial') > 0):
+            # Train discriminator only if the adversarial loss is used
+            real_loss = adv_loss(discriminator(real_data, conditions), valid)
+            fake_loss = adv_loss(discriminator(generated_data.detach(), conditions), fake)
 
-        d_loss = (real_loss + fake_loss) / 2
-        d_loss_list.append(d_loss.item())
+            d_loss = (real_loss + fake_loss) / 2
+            d_loss_list.append(d_loss.item())
+            d_loss.backward()
+            optimizer_D.step()
+        else:
+            d_loss = torch.tensor(0)
 
-        d_loss.backward()
-        optimizer_D.step()
-        optimizer_G.zero_grad()
-        g_loss = criterion(discriminator(generated_data, conditions), valid)        
-        g_mse = nn.MSELoss()(generated_data, real_data)
+        if (params.get('load_generator_path') is not None) and (params.get('load_generator_cp') is not None) and (epoch > params.get('generator_nr_freeze_epochs')):
+            # Freeze the generator if we are using a pretrained model
+            #for param in generator.parameters():
+            #    param.requires_grad = False
 
-        # Add reconstructive loss
-        # TODO: We are only testing one loss at the time right now.
-        if (params.get('lambda_mse') is not None) and (params.get('lambda_mse') > 0):
-            g_loss = g_loss + params.get('lambda_mse') * g_mse
+            optimizer_G.zero_grad()
 
-        elif (params.get('lambda_fftwass') is not None) and (params.get('lambda_fftwass') > 0):
-            loss_f = losses.FFTWassersteinLoss()
-            fft_wass_loss = loss_f(generated_data, real_data)
-            g_loss = g_loss + params.get('lambda_fftwass') * fft_wass_loss
-        elif (params.get('lambda_fftmse') is not None) and (params.get('lambda_fftmse') > 0):
-            loss_f = losses.FFTMSELoss()
-            fft_mse = loss_f(generated_data, real_data)
-            g_loss = g_loss + params.get('lambda_fftmse') * fft_mse
+            g_mse = nn.MSELoss()(generated_data, real_data)
 
+            if (params.get('lambda_adversarial') is not None) and (params.get('lambda_adversarial') > 0):
+                g_loss = params.get('lambda_adversarial') * adv_loss(discriminator(generated_data, conditions), valid)
+            else:
+                g_loss = torch.tensor(0)
+            # TODO: We are only testing one loss at the time right now.
+            if (params.get('lambda_mse') is not None) and (params.get('lambda_mse') > 0):
+                g_loss = g_loss + params.get('lambda_mse') * g_mse
+
+            elif (params.get('lambda_fftwass') is not None) and (params.get('lambda_fftwass') > 0):
+                loss_f = losses.FFTWassersteinLoss()
+                fft_wass_loss = loss_f(generated_data, real_data)
+                g_loss = g_loss + params.get('lambda_fftwass') * fft_wass_loss
+
+            elif (params.get('lambda_fftmse') is not None) and (params.get('lambda_fftmse') > 0):
+                loss_f = losses.FFTMSELoss()
+                fft_mse = loss_f(generated_data, real_data)
+                g_loss = g_loss + params.get('lambda_fftmse') * fft_mse
+
+            # TODO: Add the case where we use pretrained models
+            #if (params["model_preload"] is not None) and (params["model_preload_cp"] is not None) and (epoch > params["model_preload_cp"]):
+            g_loss.backward()
+            optimizer_G.step()
+        else:
+            g_loss = torch.tensor(0)
+            g_mse = torch.tensor(0)
+            fft_wass_loss = torch.tensor(0)
+            fft_mse = torch.tensor(0)
+        
         g_loss_list.append(g_loss.item())
-        #if (params["model_preload"] is not None) and (params["model_preload_cp"] is not None) and (epoch > params["model_preload_cp"]):
-        g_loss.backward()
-        optimizer_G.step()
+
 
     t2 = timer()
 
+    # Logs
     if (epoch % params.get('log_freq')==0):
         # TODO: The loss should be averaged over the epoch. This shows the loss for the last batch in the epoch.
-        log_out_string = f"[Epoch {epoch}/{params.get('n_epochs')}] [Time: {t2-t1:.2f}s] [D loss: {d_loss.item()}] [G loss: {g_loss.item():.4f}]"
+        log_out_string = f"[Epoch {epoch}/{params.get('n_epochs')}] [Time: {t2-t1:.2f}s] [D loss: {d_loss.item():.4f}] [G loss: {g_loss.item():.4f}]"
 
         if (params.get('lambda_mse') is not None) and (params.get('lambda_mse') > 0):
             log_out_string += f" [G fool loss: {g_loss.item()-(params.get('lambda_mse') * g_mse.item()):.4f}] [G MSE: {g_mse.item():.4f}]"
+        
         if (params.get('lambda_fftwass') is not None) and (params.get('lambda_fftwass') > 0):
             log_out_string += f" [G fool loss: {g_loss.item()-(params.get('lambda_fftwass') * fft_wass_loss.item()):.4f}] [G FFTWass: {fft_wass_loss.item():.4f}]"
+        
         if (params.get('lambda_fftmse') is not None) and (params.get('lambda_fftmse') > 0):
             log_out_string += f" [G fool loss: {g_loss.item()-(params.get('lambda_fftmse') * fft_mse.item()):.4f}] [G FFTMSE: {fft_mse.item():.4f}]"
+        
         logs.write(log_out_string + "\n")
+        print(log_out_string)
+
+    if epoch % params.get('plot_freq')==0:
         plot_gan_samples(real_data[:8].detach().cpu().numpy(), generated_data[:8].detach().cpu().numpy(), num_pairs=8, figsize=(5, 5), 
                             plot_name=f'comparison_output_{epoch}',
                             output_folder=os.path.join(output_folder, "plots"))
             
     if epoch % params.get('cp_freq')==0:
-        torch.save(generator.state_dict(), os.path.join(checkpoint_dir, f"cp-{epoch:04d}.pth"))
+        torch.save(generator.state_dict(), os.path.join(cp_dir, f"cp-{epoch:04d}.pth"))
+        if params.get('save_discr'):
+            torch.save(discriminator.state_dict(), os.path.join(cp_dir, f"cp_discr-{epoch:04d}.pth"))
         print(f"Checkpoint saved at epoch {epoch}")
 
 # Plot the losses over iterations
