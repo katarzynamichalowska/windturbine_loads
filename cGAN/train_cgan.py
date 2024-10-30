102772147e3be3069bb2486c1a567f7005e8071f
Katarzyna Michalowska <katarzyna.michalowska@sintef.no>
2024-10-30 10:54:22 +0100

    added main() and improved readability

--- cGAN/train_cgan.py
+++ cGAN/train_cgan.py
@@ -26,11 +26,13 @@ torch.backends.cudnn.benchmark = True
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Using device: {device}")
 
+# Argument parsing
 def parse_args():
     parser = argparse.ArgumentParser(description="Pass YAML configuration to the script.")
     parser.add_argument('-p', '--params', required=True, help='Path to the YAML parameters file')
     return parser.parse_args()
 
+# Parameter utilities
 def par(key, default=None):
     if key not in params and default is None:
         raise KeyError(f"Parameter '{key}' not found in params")
@@ -39,7 +41,8 @@ def par(key, default=None):
 def load_params(params_file):
     with open(os.path.join(current_dir, params_file), 'r') as file:
         return yaml.safe_load(file)
-    
+
+# Directory setup    
 def setup_output_folder():
     output_folder = f"{par('save_model_dir')}/cgan_{timestamp_now()}"
     os.makedirs(output_folder, exist_ok=True)
@@ -47,6 +50,7 @@ def setup_output_folder():
     os.makedirs(os.path.join(output_folder, "cp"), exist_ok=True)
     return output_folder
 
+# Data loading and preprocessing
 def load_and_preprocess_data():
     if isinstance(par('datasets'), list):
         X_list, Y_list, info_list = [], [], []
@@ -64,46 +68,33 @@ def load_and_preprocess_data():
 
     return X, Y, info
 
-def load_models():
-    # Generator
-    generator = md.Generator(input_features=X.shape[2], hidden_dim=par('hidden_dim'), 
-                             noise_dim=par('noise_vector_dim'), timesteps=par('t_len'))
+# Model loading
+def load_models(condition_dim, output_channels):
 
-    # Discriminator based on configuration
-    if par('skip_connections'):
-        if par('discriminator_input') == "ts":
-            discriminator = md.DiscriminatorWithSkipConnections(input_channels, condition_dim)
-        elif par('discriminator_input') == "fft":
-            discriminator = md.DiscriminatorFFTInputWithSkipConnections(input_channels, condition_dim, use_log=par('discriminator_use_log'))
-    else:
-        if par('discriminator_input') == "ts":
-            discriminator = md.Discriminator(input_channels, condition_dim)
-        elif par('discriminator_input') == "fft":
-            discriminator = md.DiscriminatorFFTInput(input_channels, condition_dim, use_log=par('discriminator_use_log'))
+    generator = md.Generator(input_features=condition_dim, hidden_dim=par('hidden_dim'), 
+                             noise_dim=par('noise_vector_dim'), timesteps=par('t_len'))
+    discriminator = (
+        md.DiscriminatorWithSkipConnections(output_channels, condition_dim)
+        if par('skip_connections') and par('discriminator_input') == "ts"
+        else md.DiscriminatorFFTInputWithSkipConnections(output_channels, condition_dim, use_log=par('discriminator_use_log'))
+        if par('skip_connections') and par('discriminator_input') == "fft"
+        else md.Discriminator(output_channels, condition_dim)
+        if par('discriminator_input') == "ts"
+        else md.DiscriminatorFFTInput(output_channels, condition_dim, use_log=par('discriminator_use_log'))
+    )
 
     return generator.to(device), discriminator.to(device)
 
+# Loading pretrained weights if specified
 def load_weights(generator, discriminator):
-    # Load weights of pretrained generator
     if (par('load_generator_path') is not None) and (par('load_generator_cp') is not None):
-        weigths_path = os.path.join(par('load_generator_path'), "cp", f"cp-{par('load_generator_cp'):04d}.pth")
-        generator.load_state_dict(torch.load(weigths_path, map_location=device))
+        generator.load_state_dict(torch.load(os.path.join(par('load_generator_path'), "cp", f"cp-{par('load_generator_cp'):04d}.pth"), map_location=device))
 
-
-    # Load weights of pretrained discriminator
     if (par('load_discriminator_path') is not None) and (par('load_discriminator_cp') is not None):
-        weigths_path = os.path.join(par('load_discriminator_path'), "cp", f"cp_discr-{par('load_discriminator_cp'):04d}.pth")
-        discriminator.load_state_dict(torch.load(weigths_path, map_location=device))
+        discriminator.load_state_dict(torch.load(os.path.join(par('load_discriminator_path'), "cp", f"cp_discr-{par('load_discriminator_cp'):04d}.pth"), map_location=device))
 
+# Plotting loss curves
 def plot_losses(d_loss_list, g_loss_list, output_folder):
-    """
-    Plots discriminator and generator losses over epochs and saves the plot.
-
-    Parameters:
-    - d_loss_list: List of discriminator losses.
-    - g_loss_list: List of generator losses.
-    - output_folder: Path to the folder where the plot will be saved.
-    """
     plt.figure(figsize=(10, 5))
     plt.plot(np.array(d_loss_list), label='Discriminator loss')
     plt.plot(np.array(g_loss_list), label='Generator loss')
@@ -114,150 +105,138 @@ def plot_losses(d_loss_list, g_loss_list, output_folder):
     plt.close()  
 
 
+# Main function to execute script
+def main():
+
+    args = parse_args()
+    global params
+    params = load_params(args.params)
+    output_folder = setup_output_folder()
+
+    logs_path = os.path.join(output_folder, "log.out")
+    logs = open(logs_path, 'w')
+    logs.write(log_functions.print_time("Program started"))
+
+    with open(os.path.join(output_folder, 'params_model.yaml'), 'w') as file:
+        yaml.dump(params, file, default_flow_style=False)
+
+    X, Y, info = load_and_preprocess_data()
+    generator, discriminator = load_models(condition_dim=X.shape[2], output_channels=Y.shape[2])
+    load_weights(generator, discriminator)
 
 
-script_dir = os.path.dirname(os.path.abspath(__file__))
+    # Optimizers
+    optimizer_G = get_optimizer(par("optimizer_G"), generator, par('lr_G'))
+    optimizer_D = get_optimizer(par("optimizer_D"), discriminator, par('lr_D'))
 
-args = parse_args()
-global params
-params = load_params(args.params)
-output_folder = setup_output_folder()
+    # Loss function
+    adv_loss = nn.BCEWithLogitsLoss()
 
-logs_path = os.path.join(output_folder, "log.out")
-logs = open(logs_path, 'w')
-logs.write(log_functions.print_time("Program started"))
+    # Create dataset and dataloader
+    print(f"Training data shapes:\nX: {X.shape}\nY: {Y.shape}")
+    dataloader = DataLoader(TensorDataset(X, Y), batch_size=par('batch_size'), shuffle=True, pin_memory=True, drop_last=True)
 
-with open(os.path.join(output_folder, 'params_model.yaml'), 'w') as file:
-    yaml.dump(params, file, default_flow_style=False)
+    # Training
+    d_loss_list, g_loss_list = [], []
+    epoch_start_training_D, epoch_start_training_G = 0, 0
 
-X, Y, info = load_and_preprocess_data()
-
-cp_dir = os.path.join(output_folder, "cp")
-
-num_samples = X.shape[0]
-timesteps = X.shape[1]
-condition_dim = X.shape[2]
-input_channels = Y.shape[2] 
-
-generator, discriminator = load_models()
-load_weights(generator, discriminator)
+    if (par('load_discriminator_path') is not None) and (par('load_discriminator_cp') is not None):
+        epoch_start_training_D = par('discriminator_nr_freeze_epochs')
+    if (par('load_generator_path') is not None) and (par('load_generator_cp') is not None):
+        epoch_start_training_G = par('generator_nr_freeze_epochs')
 
 
-# Optimizers
-optimizer_G = get_optimizer(par("optimizer_G"), generator, par('lr_G'))
-optimizer_D = get_optimizer(par("optimizer_D"), discriminator, par('lr_D'))
+    labels_real = torch.ones(par('batch_size'), 1, requires_grad=False).to(device)
+    labels_fake = torch.zeros(par('batch_size'), 1, requires_grad=False).to(device)
 
-# Loss function
-adv_loss = nn.BCEWithLogitsLoss()
+    # Define learnable parameters for the weights (initialize with 1.0 for equal contribution initially)
+    lambda_adv = par('lambda_adv')
+    lambda_mse = par('lambda_mse')
 
-# Create dataset and dataloader
-print(f"Training data shapes:\nX: {X.shape}\nY: {Y.shape}")
-dataloader = DataLoader(TensorDataset(X, Y), batch_size=par('batch_size'), shuffle=True, pin_memory=True, drop_last=True)
-
-# Training
-d_loss_list, g_loss_list = [], []
-fft_wass_loss, fft_mse = None, None
-epoch_start_training_D, epoch_start_training_G = 0, 0
-
-if (par('load_discriminator_path') is not None) and (par('load_discriminator_cp') is not None):
-    epoch_start_training_D = par('discriminator_nr_freeze_epochs')
-if (par('load_generator_path') is not None) and (par('load_generator_cp') is not None):
-    epoch_start_training_G = par('generator_nr_freeze_epochs')
-
-lambda_fftwass = par('lambda_fftwass', 0)
-lambda_fftmse = par('lambda_fftmse', 0)
-use_wgan = par('use_wgan', False)
-lambda_gp = par('lambda_gp', 0)
-
-labels_real = torch.ones(par('batch_size'), 1, requires_grad=False).to(device)
-labels_fake = torch.zeros(par('batch_size'), 1, requires_grad=False).to(device)
-
-# Define learnable parameters for the weights (initialize with 1.0 for equal contribution initially)
-lambda_adv = par('lambda_adv')
-lambda_mse = par('lambda_mse')
-
-if par('sa_weights'):
-    lambda_adv = torch.nn.Parameter(torch.tensor(0.0, device=device))  # log(s_adv) This is actually log_lambda_avd
-    lambda_mse = torch.nn.Parameter(torch.tensor(0.0, device=device))  # log(s_mse) This is actually log_lambda_mse
-    optimizer_w = torch.optim.Adam([lambda_adv, lambda_mse], lr=1e-3)
+    if par('sa_weights'):
+        lambda_adv = torch.nn.Parameter(torch.tensor(0.0, device=device))  # log(s_adv) This is actually log_lambda_avd
+        lambda_mse = torch.nn.Parameter(torch.tensor(0.0, device=device))  # log(s_mse) This is actually log_lambda_mse
+        optimizer_w = torch.optim.Adam([lambda_adv, lambda_mse], lr=1e-3)
 
 
+    d_loss = torch.tensor(0, device=device, dtype=torch.float32)
 
+    for epoch in range(1, par('n_epochs')+1):
+        t1 = timer()
+        d_accum = torch.tensor(0, device=device, dtype=torch.float32)
+        g_accum = torch.tensor(0, device=device, dtype=torch.float32)
 
-d_loss = torch.tensor(0, device=device, dtype=torch.float32)
+        for i, (conditions, real_data) in enumerate(dataloader):
+            real_data = real_data.to(device, non_blocking=True).float()
+            conditions = conditions.to(device, non_blocking=True).float()
 
-for epoch in range(1, par('n_epochs')+1):
-    t1 = timer()
-    d_accum = torch.tensor(0, device=device, dtype=torch.float32)
-    g_accum = torch.tensor(0, device=device, dtype=torch.float32)
+            optimizer_D.zero_grad()
+            generated_data = generator(conditions)
+            if par('discriminator_match_features'):
+                labels_discriminator_fake, features_output_fake = discriminator(generated_data.detach(), conditions, return_features=par('discriminator_match_features'))
+                labels_discriminator_real, features_output_real = discriminator(real_data, conditions, return_features=par('discriminator_match_features'))
+            else:
+                labels_discriminator_fake = discriminator(generated_data.detach(), conditions)
+                labels_discriminator_real = discriminator(real_data, conditions)
 
-    for i, (conditions, real_data) in enumerate(dataloader):
-        real_data = real_data.to(device, non_blocking=True).float()
-        conditions = conditions.to(device, non_blocking=True).float()
-
-        optimizer_D.zero_grad()
-        generated_data = generator(conditions)
-        if par('discriminator_match_features'):
-            labels_discriminator_fake, features_output_fake = discriminator(generated_data.detach(), conditions, return_features=par('discriminator_match_features'))
-            labels_discriminator_real, features_output_real = discriminator(real_data, conditions, return_features=par('discriminator_match_features'))
-        else:
-            labels_discriminator_fake = discriminator(generated_data.detach(), conditions)
-            labels_discriminator_real = discriminator(real_data, conditions)
-
-        if lambda_adv > 0:
-            d_loss = compute_d_loss(discriminator, real_data, generated_data, conditions,  labels_real, labels_fake, labels_discriminator_fake, labels_discriminator_real, 
-                                    lambda_gp=lambda_gp, adv_loss=adv_loss, use_wgan=use_wgan, device=device)
+            if lambda_adv > 0:
+                d_loss = compute_d_loss(discriminator, real_data, generated_data, conditions,  labels_real, labels_fake, labels_discriminator_fake, labels_discriminator_real, 
+                                        lambda_gp=par('lambda_gp'), adv_loss=adv_loss, use_wgan=par('use_wgan'), device=device)
+                
+                if epoch >= epoch_start_training_D:
+                    d_loss.backward()
+                    optimizer_D.step()            
             
-            if epoch >= epoch_start_training_D:
-                d_loss.backward()
-                optimizer_D.step()            
-        
-        optimizer_G.zero_grad()
-        if par('sa_weights'):
-            optimizer_w.zero_grad()
-
-        labels_discriminator_fake = discriminator(generated_data, conditions)
-        g_loss, g_adv, g_mse, g_fft_mse, g_fft_wass = compute_g_loss(generated_data, real_data, 
-                                                                      labels_discriminator_fake, labels_real, 
-                                                                      lambda_adv, lambda_mse, 
-                                                                      lambda_fftwass, lambda_fftmse,
-                                                                      use_wgan=use_wgan, device=device,
-                                                                      adv_loss=adv_loss, lambda_adv=lambda_adv, lambda_mse=lambda_mse,
-                                                                      sa_weights=par('sa_weights'))
-        
-
-        if epoch >= epoch_start_training_G:
-            g_loss.backward()
-            optimizer_G.step()
+            optimizer_G.zero_grad()
             if par('sa_weights'):
-                optimizer_w.step()
+                optimizer_w.zero_grad()
 
-        d_accum += d_loss
-        g_accum += g_loss
-        
-    t2 = timer()
-
-    d_loss_list.append(d_accum.item())
-    g_loss_list.append(g_accum.item())
-
-    if epoch % par('log_freq')==0:
-        log_out_string = produce_training_log(epoch, t1, t2, d_loss, g_loss, g_adv, g_mse, g_fft_mse, g_fft_wass, params)
-        logs.write(log_out_string + '\n')
-        print(log_out_string)
-
-    if epoch % par('plot_freq')==0:
-        plot_gan_samples(real_data[:8].detach().cpu().numpy(), generated_data[:8].detach().cpu().numpy(), 
-                         num_pairs=8, figsize=(5, 5), plot_name=f'comparison_output_{epoch}',
-                         output_folder=os.path.join(output_folder, "plots"))
+            labels_discriminator_fake = discriminator(generated_data, conditions)
+            g_loss, g_adv, g_mse, g_fft_mse, g_fft_wass = compute_g_loss(generated_data, real_data, 
+                                                                        labels_discriminator_fake, labels_real, 
+                                                                        lambda_adv, lambda_mse, 
+                                                                        par('lambda_fftwass'), par('lambda_fftmse'),
+                                                                        use_wgan=par('use_wgan'), device=device,
+                                                                        adv_loss=adv_loss, lambda_adv=lambda_adv, lambda_mse=lambda_mse,
+                                                                        sa_weights=par('sa_weights'))
             
-    if epoch % par('cp_freq')==0:
-        torch.save(generator.state_dict(), os.path.join(cp_dir, f"cp-{epoch:04d}.pth"))
-        if par('save_discriminator'):
-            torch.save(discriminator.state_dict(), os.path.join(cp_dir, f"cp_discr-{epoch:04d}.pth"))
-        print(f"Checkpoint saved at epoch {epoch}")
+
+            if epoch >= epoch_start_training_G:
+                g_loss.backward()
+                optimizer_G.step()
+                if par('sa_weights'):
+                    optimizer_w.step()
+
+            d_accum += d_loss
+            g_accum += g_loss
+            
+        t2 = timer()
+
+        d_loss_list.append(d_accum.item())
+        g_loss_list.append(g_accum.item())
+
+        if epoch % par('log_freq')==0:
+            log_out_string = produce_training_log(epoch, t1, t2, d_loss, g_loss, g_adv, g_mse, g_fft_mse, g_fft_wass, params)
+            logs.write(log_out_string + '\n')
+            print(log_out_string)
+
+        if epoch % par('plot_freq')==0:
+            plot_gan_samples(real_data[:8].detach().cpu().numpy(), generated_data[:8].detach().cpu().numpy(), 
+                            num_pairs=8, figsize=(5, 5), plot_name=f'comparison_output_{epoch}',
+                            output_folder=os.path.join(output_folder, "plots"))
+                
+        if epoch % par('cp_freq')==0:
+            torch.save(generator.state_dict(), os.path.join(output_folder, "cp", f"cp-{epoch:04d}.pth"))
+            if par('save_discriminator'):
+                torch.save(discriminator.state_dict(), os.path.join(output_folder, "cp", f"cp_discr-{epoch:04d}.pth"))
+            print(f"Checkpoint saved at epoch {epoch}")
 
 
-plot_losses(d_loss_list, g_loss_list, output_folder)
+    plot_losses(d_loss_list, g_loss_list, output_folder)
 
-logs.write(log_functions.print_time("Program ended"))
-logs.close()
+    logs.write(log_functions.print_time("Program ended"))
+    logs.close()
+
+# Entry point for the script
+if __name__ == "__main__":
+    main()
